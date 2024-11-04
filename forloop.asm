005d7e31d581dd1d9fd035235219f5bb668b8d12
Hansika <nostoc@vps-1db0446b.vps.ovh.ca>
2024-11-04 10:57:44 +0000

    implement for loop

--- forloop.asm
+++ forloop.asm
@@ -8,18 +8,25 @@ section .text
 global _start
 
 _start:
+    mov ecx, 0
 
     ;display *
     DISP:
+    push rcx 
+    ;------------------
     mov edx, size1     ;msg length
     mov ecx, char1   ; pointer to variable
     mov ebx, 1      ;stdout
     mov eax, 4      ;syscall number as sys_write
     int 80h
- 
-    ; call the kernel
-    jmp DISP
     
+    pop rcx
+    inc ecx
+    cmp ecx, 10
+    jl DISP
+    jg ENGPROG
+
+    ENGPROG:
     mov eax, 1
     mov ebx, 5
     int 80h
